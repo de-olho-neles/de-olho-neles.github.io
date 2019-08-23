05c957afb194a311fd7622be9d0ffb0613834165
Rodrigo Fiori <rodrigo.ferreirafiori@altran.com>
2019-08-23 13:55:09 +0100

    Adding Mocks and models for deputies info

---
+++ app/src/app/model/deputies.ts
@@ -0,0 +1,11 @@
+export interface DeputiesInfo {
+  email: string;
+  id: number;
+  idLegislatura: number;
+  nome: string;
+  siglaPartido: string;
+  siglaUf: string;
+  uri: string;
+  uriPartido: string;
+  urlFoto: string;
+}
