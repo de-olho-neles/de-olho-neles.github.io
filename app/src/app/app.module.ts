0de97f629f210a881617cc067df96450ebaed430
dtapaismorales <daniel.tapiasmorales@altran.com>
2019-08-23 10:10:41 +0100

    init routes

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import {AppMaterialModule} from './app-material/app-material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+// PAGES
 import { HomeComponent } from './Pages/home/home.component';
 import { ListComponent } from './Pages/list/list.component';
 import { LoginComponent } from './Pages/login/login.component';
-import { SearchComponent } from './Components/search/search.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppMaterialModule} from './app-material/app-material.module';
+import { SearchComponent } from './Components/Search/search.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
     HomeComponent,
     ListComponent,
-    LoginComponent,
-    SearchComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
